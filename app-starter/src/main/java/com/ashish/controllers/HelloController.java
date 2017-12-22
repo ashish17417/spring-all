80ead977d2f921cf6c759e932c659032a2d0d172
ashish17417 <ashish17417@users.noreply.github.com>
2017-12-21 20:51:20 -0500

    Changes for docker image builder

--- app-starter/src/main/java/com/ashish/controllers/HelloController.java
+++ app-starter/src/main/java/com/ashish/controllers/HelloController.java
@@ -1,6 +1,9 @@
 package com.ashish.controllers;
 
+import java.io.IOException;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +12,11 @@ public class HelloController {
 	
 	@GetMapping
 	public String hellow() {
+		return "hellow";
+	}
+	
+	@GetMapping("/create")
+	public String createFile(@RequestParam int num) throws IOException {
 		return "hellow";
 	}
 }
